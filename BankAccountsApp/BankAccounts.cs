d58f9b31ee51cc1ee3c6c3a3cb129a110569f7b1
raad_ <raad_asded@hotmail.com>
2025-02-17 13:26:45 +0200

    Add project files.

---
+++ BankAccountsApp/BankAccounts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountsApp
+{
+    public class BankAccounts
+    {
+        public string Owner {  get; set; }
+        public Guid AccountNumber {  get; set; }
+        public decimal Balance {  get; set; }
+    }
+}
---
+++ BankAccountsApp/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace BankAccountsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            OwnerTxt = new TextBox();
+            AmountNum = new NumericUpDown();
+            BankAccountsGrid = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)AmountNum).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BankAccountsGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 21);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Owner";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(0, 290);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Amount";
+            // 
+            // OwnerTxt
+            // 
+            OwnerTxt.Location = new Point(96, 21);
+            OwnerTxt.Name = "OwnerTxt";
+            OwnerTxt.Size = new Size(160, 35);
+            OwnerTxt.TabIndex = 2;
+            // 
+            // AmountNum
+            // 
+            AmountNum.Location = new Point(96, 288);
+            AmountNum.Name = "AmountNum";
+            AmountNum.Size = new Size(154, 35);
+            AmountNum.TabIndex = 3;
+            // 
+            // BankAccountsGrid
+            // 
+            BankAccountsGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            BankAccountsGrid.Location = new Point(277, 21);
+            BankAccountsGrid.Name = "BankAccountsGrid";
+            BankAccountsGrid.Size = new Size(353, 245);
+            BankAccountsGrid.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(277, 272);
+            button1.Name = "button1";
+            button1.Size = new Size(170, 51);
+            button1.TabIndex = 5;
+            button1.Text = "Deposit";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(453, 272);
+            button2.Name = "button2";
+            button2.Size = new Size(177, 51);
+            button2.TabIndex = 6;
+            button2.Text = "Withdraw";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(96, 62);
+            button3.Name = "button3";
+            button3.Size = new Size(160, 48);
+            button3.TabIndex = 7;
+            button3.Text = "create account";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(635, 329);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(BankAccountsGrid);
+            Controls.Add(AmountNum);
+            Controls.Add(OwnerTxt);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form1";
+            Text = "Owner";
+            ((System.ComponentModel.ISupportInitialize)AmountNum).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BankAccountsGrid).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox OwnerTxt;
+        private NumericUpDown AmountNum;
+        private DataGridView BankAccountsGrid;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
